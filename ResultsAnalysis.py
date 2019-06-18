0fc64b7da8db16704bd440a15cbd32e8805a9717
stacy-s <stacey.stepanova@gmail.com>
2019-06-18 21:01:43 +0400

    An experiment module has been added.

---
+++ ResultsAnalysis.py
@@ -0,0 +1,36 @@
+import Cluster
+import Clustering
+import Metrics
+import numpy as np
+import BuildData
+from sklearn.datasets import make_moons, make_blobs, make_circles
+import time
+
+def static_experiment(metric, count_experiments, k_range, eps_range, function_get_dataset):
+    size = (count_experiments * len(k_range) * len(eps_range), 4)
+    experimental_results = np.zeros(shape=size, dtype=float)
+    row = 0
+    for i in range(count_experiments):
+        print(i)
+        points, correct_clustering = function_get_dataset()
+        for k in k_range:
+            # start = time.time()
+            for eps in eps_range:
+                d = Cluster.Cluster(points, correct_clustering)
+                mxt = Clustering.K_MXT(k=k, eps=eps, cluster=d)
+                mxt()
+                score_mxt = metric(mxt)
+                gauss = Clustering.K_MXTGauss(k=k, eps=eps, cluster=d)
+                gauss()
+                score_gauss = metric(gauss)
+                experimental_results[row][0], experimental_results[row][1] = k, eps
+                experimental_results[row][2], experimental_results[row][3] = score_mxt(), score_gauss()
+                row += 1
+            # end = time.time()
+            # print(end - start)
+    return experimental_results
+
+
+def save_results(func_name, experimental_results):
+    np.savetxt(func_name, experimental_results, header='k   eps   k-MXT   k_MXT-Gauss', fmt='%d %.2f %.2f %.2f')
+
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import Clustering
 import Metrics
 import numpy as np
 import BuildData
+import ResultsAnalysis
 from sklearn.datasets import make_moons, make_blobs, make_circles
 import time
+import functools
 
 
 def build_plot_metrics_eps(k, eps: np.ndarray, algorithm_clustering, metric, cluster: Cluster.Cluster, step=0.5,
@@ -52,6 +54,17 @@ def build_plot_metrics_eps(k, eps: np.ndarray, algorithm_clustering, metric, clu
 
 
 def main():
+    data = [functools.partial(make_blobs, n_samples=200, cluster_std=0.5),
+            functools.partial(make_blobs, n_samples=200, cluster_std=[1.0, 1.5, 0.5]),
+            functools.partial(make_circles, n_samples=200, noise=0.05, factor=0.4),
+            functools.partial(make_moons, n_samples=200, noise=0.05)]  # наборы данных
+    for i in range(len(data)):
+        print(data[i].__str__())
+        experimental_results = ResultsAnalysis.static_experiment(Metrics.ARI, 1000,
+                                                                 np.arange(1, 13), np.arange(0.1, 4.1, 0.1), data[i])
+        ResultsAnalysis.save_results(f"{data[i].func.__name__} {data[i].__str__()[data[i].__str__().find(',') + 2:-1]}",
+                                     experimental_results)
+
     # data = [make_moons(n_samples=200, noise=0.05, random_state=0),
     #         make_circles(n_samples=200, noise=0.05, random_state=0, factor=0.4),
     #         make_blobs(n_samples=200, random_state=0, cluster_std=0.5),
@@ -60,28 +73,29 @@ def main():
     # # Запуск алгоритма k-MXT для всех указанных значений k и eps для каждого набора данных.
     # # Построение графика зависимости метрики ARI от значений eps для каждого k.
     # # Отрисовка лучшего разбиения на кластеры для каждого k.
-    # for points, correct_clustering in data:
-    #     d = Cluster.Cluster(points, correct_clustering)
+    # # for points, correct_clustering in data:
+    # for points, ccorrect_clustering in data:
+    #     d = Cluster.Cluster(points, ccorrect_clustering)
     #     for k in range(1, 13):
     #         build_plot_metrics_eps(k, np.arange(0.1, 4.1, 0.1), Clustering.K_MXT, Metrics.ARI, d)
     #         build_plot_metrics_eps(k, np.arange(0.1, 4.1, 0.1), Clustering.K_MXTGauss, Metrics.ARI, d)
-    #
+
     # # Кластеризация данных для города Санкт-Петербурга при заданных значениях параметров k и eps.
-    d = Cluster.ClusterGreatCircles('~/documents/diplom/', 'geoflickr_spt.csv')
-    for k in [7]:
-        for eps in [50]:
-            c = Clustering.K_MXTGreatCircle(eps, k, d)
-            c()
-            m = Metrics.Modularity(c)
-            print('k-MXT ', m(), k, eps)
-            c.cluster.view_at_map(latitude=59.93863, longitude=30.31413,
-                                  filename_of_map=f'{k}-MXT-eps{eps}')
-            c = Clustering.K_MXTGaussGreatCircle(eps, k, d)
-            c()
-            c.cluster.view_at_map(latitude=59.93863, longitude=30.31413,
-                                  filename_of_map=f'{k}-MXTGauss-eps{eps}')
-            m = Metrics.Modularity(c)
-            print('Gauss ', m(), k, eps)
+    # d = Cluster.ClusterGreatCircles('~/documents/diplom/', 'geoflickr_spt.csv')
+    # for k in [7]:
+    #     for eps in [50]:
+    #         c = Clustering.K_MXTGreatCircle(eps, k, d)
+    #         c()
+    #         m = Metrics.Modularity(c)
+    #         print('k-MXT ', m(), k, eps)
+    #         c.cluster.view_at_map(latitude=59.93863, longitude=30.31413,
+    #                               filename_of_map=f'{k}-MXT-eps{eps}')
+    #         c = Clustering.K_MXTGaussGreatCircle(eps, k, d)
+    #         c()
+    #         c.cluster.view_at_map(latitude=59.93863, longitude=30.31413,
+    #                               filename_of_map=f'{k}-MXTGauss-eps{eps}')
+    #         m = Metrics.Modularity(c)
+    #         print('Gauss ', m(), k, eps)
 
 
 
